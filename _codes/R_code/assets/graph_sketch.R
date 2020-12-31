761609fc62053ff2f1f687f09a953925d9039230
odunayo12 <odun.rotimi@gmail.com>
2020-12-31 15:09:43 +0100

    rematch graph to topology

--- _codes/R_code/assets/graph_sketch.R
+++ _codes/R_code/assets/graph_sketch.R
@@ -1,5 +1,8 @@
 library(igraph)
 library(tidyverse)
+library(ggraph)
+library(tidygraph)
+library(ggforce)
 adj_mat <- c(0, 1.2, 2, 0.7, 1, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
 1.2, 0, 2, 2, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
 2, 2, 0, 3, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 
@@ -33,11 +36,46 @@ the_adj_mat <- matrix(adj_mat, nc =26, nr=26)
 g_init <- graph.adjacency(the_adj_mat, weighted = T)
 #duplicate
 df <- get.data.frame(g_init) 
-df_tibble %>% group_by(from) %>%mutate( id_= cur_group_id()) %>% view()
+df = df%>% group_by(from) %>%mutate( id_= cur_group_id()) %>% view()
 
 # build the graph object
 #network <- graph_from_data_frame(d=df, directed=F) 
-network <- graph_from_adjacency_matrix(the_adj_mat,weighted = T, mode = "undirected", diag = F)
+network <- graph_from_adjacency_matrix(the_adj_mat,weighted = T, diag = F)
 #graph_fr
 # plot it
-plot(network, layout=layout_on_grid)
+
+# working
+#plot(network, layout=layout_on_grid)
+
+graph_df <- graph_from_data_frame(df)
+layout <- create_layout(graph_df, layout = 'stress')
+# change the latitude here in x and y, label within group with the id
+layout_edit <-
+  layout %>% mutate(
+    x = case_when(
+      .ggraph.orig_index > 20 ~ sample(1:100, 26),
+      .ggraph.orig_index <= 10 ~ sample(45:100, 26),
+      .ggraph.orig_index > 10 &
+        .ggraph.orig_index <= 20 ~ sample(25:120, 26)
+    ),
+    y = case_when(
+      .ggraph.orig_index > 20 ~ sample(101:1000, 26),
+      .ggraph.orig_index <= 10 ~ sample(450:1000, 26),
+      .ggraph.orig_index > 10 &
+        .ggraph.orig_index <= 20 ~ sample(256:1205, 26)
+    ),
+    id = case_when(
+      .ggraph.orig_index > 20 ~ "3",
+      .ggraph.orig_index > 10 &
+        .ggraph.orig_index <= 20 ~ "2",
+      .ggraph.orig_index <= 10 ~ "1"
+      
+    )
+  ) %>% view()
+ggraph(layout_edit) +
+  geom_edge_link() +
+  geom_node_point(aes(colour = id)) +
+  geom_node_text(aes(label = as.character(.ggraph.orig_index))) +
+  theme_classic()
+head(layout)
+layout$.ggraph.orig_index
