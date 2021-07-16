f3ba71d62e746aa1dff49860450aeb664f551df8
odunayo12 <odun.rotimi@gmail.com>
2021-07-16 15:00:05 +0200

    add edge functions

--- _codes/SIR/Influential-nodes-identification-in-complex-networks-via-information-entropy/experiments and data/algo_rank.py
+++ _codes/SIR/Influential-nodes-identification-in-complex-networks-via-information-entropy/experiments and data/algo_rank.py
@@ -13,7 +13,7 @@ sys.path.append(r'c:\\Users\\rotim\\OneDrive\\Documents\\Reading\\graph-code\\Sk
 
 # from algorithms import *  # isort:skip
 # %%
-data_file = 'CEnew'  # 'HepPh'
+data_file = 'topo'  # 'CEnew'  # 'HepPh'
 G = nx.read_adjlist(data_file)
 G.remove_edges_from(nx.selfloop_edges(G))
 nodes = list(nx.nodes(G))
@@ -21,6 +21,28 @@ for node in nodes:
     if G.degree(node) == 0:
         G.remove_node(node)
 print(nx.number_of_nodes(G), nx.number_of_edges(G))
+# %%
+# draw graph
+nx.draw(G, with_labels=True)
+
+# draw degree dsitribution
+
+
+def plot_degree_dist(G, n):
+    node = list(G.degree())
+    degrees = [len(n_neighbor(G, i, n)) for (i, j) in node]
+    plt.hist(degrees, bins=50)
+    plt.show()
+# %%
+
+
+# draw n-degree dsitribution
+# node = list(G.degree())#[:5]
+# node_k_2 = [(i, len(n_neighbor(G, i, 2))) for (i, j) in node]
+plot_degree_dist(G, 1)
+plot_degree_dist(G, 2)
+plot_degree_dist(G, 3)
+
 # %%
 max_ = 0.016  # %age of nodes to be selected
 max_topk = round(max_ * nx.number_of_nodes(G))  # number of nodes selected
@@ -94,3 +116,54 @@ df["b'"] = pd.DataFrame([df["a"], df["b"]]).max()
 df = df.groupby(by=["a'", "b'"]).sum().reset_index()
 G = nx.from_pandas_edgelist(df, "a'", "b'", ['weight'])
 [G[u][v]['weight'] for u, v in G.edges()]
+
+[x for x in ['a', 'e', 'i', 'o', 'u'] if x not in ['a']]
+# %%
+
+
+def n_neighbor(g, id, n_hop):
+    node = [id]
+    node_visited = set()
+    neighbors = []
+
+    while n_hop != 0:
+        neighbors = []
+        for node_id in node:
+            node_visited.add(node_id)
+            neighbors += [id for id in g.neighbors(node_id)
+                          if id not in node_visited]
+        node = neighbors
+        n_hop -= 1
+
+        if len(node) == 0:
+            return neighbors
+
+    return neighbors
+
+
+# %%
+n_neighbor(G, '1', 2)
+# %%
+n_neighbor(G, '1', 1)
+
+# %%
+n_neighbor(G, '2', 1)
+
+# get node and neighbors
+node_n_nbr = [(i, n_neighbor(G, i, 2)) for (i, j) in node]
+# %%
+# shows the nodes that connecst 2 parts so far a path exists between them.
+#  [1:-1] removes the start and end node e.g. removes "1" and "3"
+# Useful for connecting nodes selcted by hubs.
+[x[1:-1] for x in nx.edge_disjoint_paths(G, "1", "3")]
+
+# displays the edge data in as (node, destination, {weight})
+G.edges(data=True)
+
+# initiate weights by setting all to 1
+nx.set_edge_attributes(G, values=1, name='weight')
+
+# assign weights dynamically
+for u, v, d in G.edges(data=True):
+    d['weight'] = int(v)
+# %%
